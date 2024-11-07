f9354fccf42ab09855aa6cdd18faf90ceaab8291
New User <newuser@example.com>
2024-11-07 21:02:48 +0300

    fixed some bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,6 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-      </head>
       <body className={inter.className} suppressHydrationWarning>
         <ClerkProvider>
           <SidebarProvider>
